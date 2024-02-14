0307740a0467699427d287658011867b3f67d2bf
Nilson Sierota Correia <nilsoncv2@gmail.com>
2024-02-13 22:56:03 -0400

    feat: Vote On Poll

---
+++ src/http/routes/vote-on-poll.ts
@@ -0,0 +1,20 @@
+import z from "zod"
+import { prisma } from "../../lib/prisma"
+import { FastifyInstance } from "fastify"
+
+export async function voteOnPoll(app: FastifyInstance) {
+  app.post('/polls/:pollId/votes', async (request, reply) => {
+    const voteOnPollBody = z.object({
+      pollOptionId: z.string().uuid(),
+    })
+
+    const voteOnPollParams = z.object({
+      pollId: z.string().uuid(),
+    })
+
+    const { pollId } = voteOnPollParams.parse(request.params)
+    const { pollOptionId } = voteOnPollBody.parse(request.body)
+
+    return reply.status(201).send()
+  })
+}
