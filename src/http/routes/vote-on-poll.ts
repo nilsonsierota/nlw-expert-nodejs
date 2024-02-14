89d887ed4ba71580c6008028111f033d97c1ec63
Nilson Sierota Correia <nilsoncv2@gmail.com>
2024-02-13 23:20:30 -0400

    feat: Vote On Poll

--- src/http/routes/vote-on-poll.ts
+++ src/http/routes/vote-on-poll.ts
@@ -1,5 +1,6 @@
 import z from "zod"
 import { FastifyInstance } from "fastify"
+import { randomUUID } from "crypto"
 
 export async function voteOnPoll(app: FastifyInstance) {
   app.post('/polls/:pollId/votes', async (request, reply) => {
@@ -13,6 +14,19 @@ export async function voteOnPoll(app: FastifyInstance) {
 
     const { pollId } = voteOnPollParams.parse(request.params)
     const { pollOptionId } = voteOnPollBody.parse(request.body)
+
+    let { sessionId } = request.cookies
+
+    if (!sessionId) {
+      sessionId = randomUUID()
+
+      reply.setCookie('sessionId', sessionId, {
+        path: '/',
+        maxAge: 60 * 60 * 24 * 30, // 30 days
+        signed: true,
+        httpOnly: true,
+      })
+    }
 
     return reply.status(201).send()
   })
